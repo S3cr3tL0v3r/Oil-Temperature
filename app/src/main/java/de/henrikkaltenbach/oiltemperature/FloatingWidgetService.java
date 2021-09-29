001ede8699c7a42bd3577880d7b185484469e94a
Henrik Kaltenbach <henrik.kaltenbach49@gmail.com>
2021-09-29 14:15:39 +0200

    Initial commit

---
+++ app/src/main/java/de/henrikkaltenbach/oiltemperature/FloatingWidgetService.java
@@ -0,0 +1,231 @@
+package de.henrikkaltenbach.oiltemperature;
+
+import android.app.Service;
+import android.bluetooth.*;
+import android.bluetooth.le.*;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.*;
+import android.util.Log;
+import android.view.*;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.UUID;
+
+public class FloatingWidgetService extends Service {
+
+    public static final String BROADCAST_ACTION = "floating widget";
+
+    private static final String OUTER_TAG = FloatingWidgetService.class.getSimpleName();
+    private static final int MAX_CLICK_DURATION = 200;
+    private static final long SCAN_PERIOD = 10000;
+    private static final String ENVIRONMENTAL_SENSING_SERVICE_UUID = "0000181A-0000-1000-8000-00805F9B34FB";
+    private static final String TEMPERATURE_CHARACTERISTIC_UUID = "00002A6E-0000-1000-8000-00805F9B34FB";
+    private static final String CCC_DESCRIPTOR_UUID = "00002902-0000-1000-8000-00805F9B34FB";
+    private static final String DEVICE_NAME = "Henrik's ESP32";
+
+    private boolean isScanning;
+    private long startClickTime;
+    private Context context;
+    private WindowManager windowManager;
+    private View floatingWidget;
+    private TextView tvOilTemperature;
+    private BluetoothLeScanner bleScanner;
+    private ScanFilter scanFilter;
+    private ScanSettings scanSettings;
+    private Handler handler;
+    private BluetoothGatt bleGatt;
+
+    public FloatingWidgetService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.v(OUTER_TAG, "onStartCommand() betreten");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.v(OUTER_TAG, "onBind() betreten");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(OUTER_TAG, "1. Stufe: onCreate() betreten");
+        floatingWidget = LayoutInflater.from(this).inflate(R.layout.service_floating_widget, null);
+        context = this;
+        scanFilter = buildScanFilter();
+        scanSettings = buildScanSettings();
+        handler = new Handler(Looper.getMainLooper());
+        final WindowManager.LayoutParams params = createLayoutParams();
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        windowManager.addView(floatingWidget, params);
+        floatingWidget.findViewById(R.id.root_container).setOnTouchListener(getFloatingWidgetOnTouchListener(params));
+        tvOilTemperature = floatingWidget.findViewById(R.id.tvOilTemperature);
+        onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v(OUTER_TAG, "6. Stufe: onDestroy() betreten");
+        if (floatingWidget != null) windowManager.removeView(floatingWidget);
+    }
+
+    private void onResume() {
+        if (getBluetoothAdapter().isEnabled()) {
+            bleScanner = getBluetoothAdapter().getBluetoothLeScanner();
+            startBleScan();
+        }
+    }
+
+    private final ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            bleScanner.stopScan(scanCallback);
+            result.getDevice().connectGatt(context, false, gattCallback);
+            BluetoothDevice bleDevice = result.getDevice();
+            Log.i( "ScanCallback", String.format("Connect to %s (%s)", bleDevice.getName(), bleDevice.getAddress()));
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.e("ScanCallback", "onScanFailed: code " + errorCode);
+        }
+    };
+
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            BluetoothDevice bleDevice = gatt.getDevice();
+            if (status == BluetoothGatt.GATT_SUCCESS && newState == BluetoothProfile.STATE_CONNECTED) {
+                bleGatt = gatt;
+                Log.i("BluetoothGattCallback", String.format("Successfully connected to %s (%s)", bleDevice.getName(), bleDevice.getAddress()));
+                handler.post(() -> bleGatt.discoverServices());
+                Log.i("BluetoothGattCallback", String.format("Discover services on %s (%s)", bleDevice.getName(), bleDevice.getAddress()));
+            } else  {
+                gatt.close();
+                handler.post(() -> tvOilTemperature.setText(R.string.n_a));
+                startBleScan();
+                Log.e("BluetoothGattCallback",  String.format("Successfully disconnected from or connection error with %s (%s)", bleDevice.getName(), bleDevice.getAddress()));
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            bleGatt = gatt;
+            BluetoothGattCharacteristic characteristic = bleGatt
+                    .getService(UUID.fromString(ENVIRONMENTAL_SENSING_SERVICE_UUID))
+                    .getCharacteristic(UUID.fromString(TEMPERATURE_CHARACTERISTIC_UUID));
+            UUID cccdUUID = UUID.fromString(CCC_DESCRIPTOR_UUID);
+            BluetoothGattDescriptor descriptor = characteristic.getDescriptor(cccdUUID);
+            descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+            if (bleGatt.setCharacteristicNotification(characteristic, true)) {
+                bleGatt.writeDescriptor(descriptor);
+                Log.i("BluetoothGattCallback", "Successfully subscribed to characteristic notifications");
+            } else {
+                Log.e("BluetoothGattCallback", "Could not subscribe to characteristic notifications");
+            }
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            byte value = characteristic.getValue()[0];
+            final String oilTemperature = String.valueOf(value);
+            handler.post(() -> tvOilTemperature.setText(oilTemperature));
+            Log.i("BluetoothGattCallback", "Characteristic value changed. Oil temperature: " + oilTemperature);
+        }
+    };
+
+    private BluetoothAdapter getBluetoothAdapter() {
+        return ((BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
+    }
+
+    private ScanFilter buildScanFilter() {
+        return new ScanFilter.Builder()
+                .setDeviceName(DEVICE_NAME)
+                .setServiceUuid(ParcelUuid.fromString(ENVIRONMENTAL_SENSING_SERVICE_UUID))
+                .build();
+    }
+
+    private ScanSettings buildScanSettings() {
+        return new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .setCallbackType(ScanSettings.CALLBACK_TYPE_FIRST_MATCH)
+                .build();
+    }
+
+    private void startBleScan() {
+        if (!isScanning) {
+            bleScanner.startScan(Collections.singletonList(scanFilter), scanSettings, scanCallback);
+            isScanning = true;
+            handler.postDelayed(this::stopBleScan, SCAN_PERIOD);
+            Log.i("BluetoothLeScanner", "Scan started");
+        }
+    }
+
+    private void stopBleScan() {
+        if (isScanning) {
+            bleScanner.stopScan(scanCallback);
+            isScanning = false;
+            Log.i("BluetoothLeScanner", "Scan stopped");
+        }
+    }
+
+    private WindowManager.LayoutParams createLayoutParams() {
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+                        ? WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                        : WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.START;
+        params.x = 0;
+        params.y = 100;
+        return params;
+    }
+
+    private View.OnTouchListener getFloatingWidgetOnTouchListener(WindowManager.LayoutParams params) {
+        return new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        startClickTime = System.currentTimeMillis();
+                        return false;
+                    case MotionEvent.ACTION_UP:
+                        int dX = (int) (event.getRawX() - initialTouchX);
+                        int dY = (int) (event.getRawY() - initialTouchY);
+                        long clickDuration = System.currentTimeMillis() - startClickTime;
+                        if (clickDuration < MAX_CLICK_DURATION) {
+                            Intent intent = new Intent(BROADCAST_ACTION);
+                            sendBroadcast(intent);
+                            stopSelf();
+                        }
+                        return false;
+                    case MotionEvent.ACTION_MOVE:
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(floatingWidget, params);
+                        return false;
+                }
+                return false;
+            }
+        };
+    }
+}
---
+++ app/src/main/java/de/henrikkaltenbach/oiltemperature/MainActivity.java
@@ -0,0 +1,137 @@
+package de.henrikkaltenbach.oiltemperature;
+
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.*;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.util.Log;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends Activity {
+
+    private static final int ENABLE_BLUETOOTH_REQUEST_CODE = 1;
+    private static final int REQUEST_CODE_FOR_OVERLAY_SCREEN = 106;
+    private static final String OUTER_TAG = MainActivity.class.getSimpleName();
+
+    private class GetFloatingIconClick extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent selfIntent = new Intent(MainActivity.this, MainActivity.class);
+            selfIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+                    | Intent.FLAG_ACTIVITY_SINGLE_TOP
+                    | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(selfIntent);
+        }
+    }
+
+    private GetFloatingIconClick receiver;
+    private final IntentFilter filter = new IntentFilter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.v(OUTER_TAG, "1. Stufe: onCreate() betreten");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(OUTER_TAG, "2. Stufe: onStart() betreten");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.v(OUTER_TAG, "3. Stufe: onResume() betreten");
+        receiver = new GetFloatingIconClick();
+        filter.addAction(FloatingWidgetService.BROADCAST_ACTION);
+        registerReceiver(receiver, filter);
+        if (!getBluetoothAdapter().isEnabled()) {
+            promptEnableBluetooth();
+        } else {
+            startService();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(OUTER_TAG, "4. Stufe: onPause() betreten");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(OUTER_TAG, "5. Stufe: onStop() betreten");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v(OUTER_TAG, "Wiederaufnahme: onRestart() betreten [nach Stufe 5, vor Stufe 2]");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(OUTER_TAG, "6. Stufe: onDestroy() betreten");
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ENABLE_BLUETOOTH_REQUEST_CODE && resultCode != Activity.RESULT_OK) {
+            promptEnableBluetooth();
+        }
+    }
+
+    private void startService() {
+        if (!hasRequiredPermissions()) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_ADMIN,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    },
+                    PackageManager.PERMISSION_GRANTED);
+        }
+        if (Settings.canDrawOverlays(MainActivity.this)) {
+            Intent startIntent = new Intent(MainActivity.this, FloatingWidgetService.class);
+            startService(startIntent);
+        } else {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, REQUEST_CODE_FOR_OVERLAY_SCREEN);
+        }
+    }
+
+    private void promptEnableBluetooth() {
+        if (!getBluetoothAdapter().isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE);
+        }
+    }
+
+    private BluetoothAdapter getBluetoothAdapter() {
+        return ((BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
+    }
+
+    private boolean hasRequiredPermissions() {
+        return hasPermission(Manifest.permission.BLUETOOTH)
+                && hasPermission(Manifest.permission.BLUETOOTH_ADMIN)
+                && hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
+    }
+
+    private boolean hasPermission(String permission) {
+        return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+}
